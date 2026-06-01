527394c055218332d8a7afe5c3163798a0c8dc7d
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2026-06-01 14:10:58 +0530

    added ratelimiter and centralized jwt checker

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -3,23 +3,26 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const authProxy = require("./routes/authProxy");
+const limiter = require("./middleware/rateLimiter");
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-console.log("AUTH_SERVICE_URL =", process.env.AUTH_SERVICE_URL);
+// console.log("AUTH_SERVICE_URL =", process.env.AUTH_SERVICE_URL);
 // app.use(express.json());
+
+app.use(limiter);
 
 app.use(cors());
 
 app.use(morgan("dev"));
 
-app.use("/api/v1/auth", (req, res, next) => {
-  console.log("AUTH ROUTE HIT");
-  next();
-});
+// app.use("/api/v1/auth", (req, res, next) => {
+//   console.log("AUTH ROUTE HIT");
+//   next();
+// });
 
 
 app.use("/api/v1/auth", authProxy);
@@ -35,7 +38,3 @@ app.listen(PORT, () => {
   console.log(`Gateway running on ${PORT}`);
 });
 
-app.post("/test", (req, res) => {
-  console.log("TEST HIT");
-  res.json({ success: true });
-});
